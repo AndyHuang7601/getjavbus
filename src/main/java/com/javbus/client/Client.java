2169325a03a0f67f7f3295c463f609199ae1ed0f
zzx <piofcircle@gmail.com>
2018-03-23 23:07:12 +0800

    乱码

--- src/main/java/com/javbus/client/Client.java
+++ src/main/java/com/javbus/client/Client.java
@@ -23,16 +23,17 @@ public class Client {
                 .url(url)
                 .get()
                 .addHeader("dnt", "1")
-                .addHeader("accept-encoding", "deflate")
+                .addHeader("accept-encoding", "plain text")
                 .addHeader("accept-language", "zh-CN,zh;q=0.9,en;q=0.8,zh-TW;q=0.7")
                 .addHeader("upgrade-insecure-requests", "1")
                 .addHeader("user-agent", "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/64.0.3282.167 Chrome/64.0.3282.167 Safari/537.36")
                 .addHeader("accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8")
                 .addHeader("cache-control", "no-cache")
-                .addHeader("authority", "www.javbus.in")
-                .addHeader("cookie", "__cfduid=d5d0ad495ab2106c3c26b42919609fc801520179612; PHPSESSID=l4cuq78ul414dsaofsmgmju0d1; HstCfa3159616=1520179617228; HstCmu3159616=1520179617228; __dtsu=2DE7B66BA3199C5ACB0C108602E20703; HstCnv3159616=3; HstCns3159616=3; HstCla3159616=1520612205132; HstPn3159616=10; HstPt3159616=59")
-                .addHeader("postman-token", "5c4f16b7-3ac9-2de6-5964-69416f1f6618")
+                .addHeader("cookie", "__utma=1.1653959757.1520007802.1520007802.1520007802.1; __utmc=1; __utmz=1.1520007802.1.1.utmcsr=(direct)|utmccn=(direct)|utmcmd=(none); cdb3_sid=RiZLvJ")
+                .addHeader("proxy-connection", "keep-alive")
+                .addHeader("postman-token", "154708c8-0206-13d7-6b19-c32192dd920a")
                 .build();
+
 
         Response response = client.newCall(request).execute();
 
--- src/test/java/com/javbus/client/ClientTest.java
+++ src/test/java/com/javbus/client/ClientTest.java
@@ -7,10 +7,13 @@ import java.io.IOException;
 public class ClientTest extends TestCase {
 
     public void testClient() throws IOException {
-        System.out.println(Client.get("https://www.javbus.in/"));
+        System.out.println(Client.get("http://67.220.90.4/bbs/forumdisplay.php?fid=143&filter=digest"));
     }
 
     public void testClient2() throws IOException {
-        System.out.println(Client.get("https://www.javbus.in/SCOP-502"));
+        String pagecontent = Client.get("http://67.220.90.4/bbs/forumdisplay.php?fid=143&filter=digest");
+        byte[] bytes = pagecontent.getBytes(("ISO-8859-1"));
+        String utf8 = new String(bytes, "GBK");
+        System.out.println(utf8);
     }
 }
